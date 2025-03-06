34147e7ee62d952937e21f27bb1a4e697a186b9b
Sercan-Ayvaz <sercanayvzz@gmail.com>
2025-03-06 14:03:59 +0300

    JSONExcanhgeRateStudies Created

--- JSONExcanhgeRateStudies.py
+++ JSONExcanhgeRateStudies.py
@@ -1,62 +1,6 @@
 import requests
 import json
 
-apiKey = '3ede2355da2f43e921590dc5'
-apiUrl = f'https://v6.exchangerate-api.com/v6/{apiKey}/latest/'
-
-
-def exchangeRateCalculate(excnageRate):
-    result = requests.get(excnageRate)
-    resultJSON = json.loads(result.text)
-    return resultJSON
-
-
-while True:
-    try:
-        while True:
-            resultChoices = input("""************************** Exchange Rate Calculate **************************
-                                \n1 - TRY - USD\n2 - TRY - EUR\n3 - EUR - TRY\n4 - EUR - USD\n5 - USD - TRY\n6 - USD - EUR\n7 - Exit\nExchange Choices: """)
-            if resultChoices == '7':
-                break
-            else:
-                tryCurrency = 'TRY'
-                usdCurrency = 'USD'
-                eurCurrency = 'EUR'
-                amount = input('Enter the amount of currency you are converting: ')
-                if resultChoices == '1':
-                    result = exchangeRateCalculate(apiUrl+tryCurrency)
-                    print(f'{amount} TRY = {result['conversion_rates']["USD"]} USD')
-                elif resultChoices == '2':
-                    result = exchangeRateCalculate(apiUrl+tryCurrency)
-                    print(f'{amount} TRY = {result['conversion_rates']["EUR"]} EUR')
-                elif resultChoices == '3':
-                    result = exchangeRateCalculate(apiUrl+eurCurrency)
-                    print(f'{amount} EUR = {result['conversion_rates']["TRY"]} TRY')
-                elif resultChoices == '4':
-                    result = exchangeRateCalculate(apiUrl+eurCurrency)
-                    print(f'{amount} EUR = {result['conversion_rates']["USD"]} USD')
-                elif resultChoices == '5':
-                    result = exchangeRateCalculate(apiUrl+usdCurrency)
-                    print(f'{amount} USD = {result['conversion_rates']["TRY"]} TRY')
-                elif resultChoices == '6':
-                    paresult = exchangeRateCalculate(apiUrl+usdCurrency)
-                    print(f'{amount} USD = {result['conversion_rates']["EUR"]} EUR')
-                else:
-                    print('Please enter a valid value!')
-    except Exception as ex:
-        print(f'Exception: {ex}')
-            
-
-
-
-
-
-
-
-
-import requests
-import json
-
 
 apiKey = '3ede2355da2f43e921590dc5' # API anahtarını tanımlıyoruz # Defining the API key
 apiUrl = f'https://v6.exchangerate-api.com/v6/{apiKey}/latest/'# API URL'sini oluşturuyoruz # Creating the API URL
